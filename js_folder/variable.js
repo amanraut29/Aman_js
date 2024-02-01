674351c0f525a3e5b6cf9f70890f19047ce7d0bd
amanraut29 <123371627+amanraut29@users.noreply.github.com>
2024-02-01 12:30:24 +0000

    save it

---
+++ js_folder/variable.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+//accountId=2 //not allowed
+//var is not good in terms of scope in program
+
+ accountEmail="rohan@google.com"
+ accountPassword="12345"
+ accountCity="jaipur2"
+
+
+ console.log([accountEmail,accountId,accountPassword,accountState,accountCity]);
+
