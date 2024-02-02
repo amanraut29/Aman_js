947ee15d41c227405d6b1cca7018b795856a84a9
amanraut29 <123371627+amanraut29@users.noreply.github.com>
2024-02-02 08:30:58 +0000

    ConversionOperation

---
+++ js_folder/conversionOperation.js
@@ -0,0 +1,26 @@
+let score="33aaaaabc"
+
+console.log(typeof score);
+console.log(typeof  (score));
+
+let valueInNumber=String(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>NaN
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn=1
+let  booleanIsLooggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLooggedIn);
+
+//******************operations************
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((3+4)*5%3);
+
+
+
